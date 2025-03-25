198b5261923a242817741164a6f3a18b6c7d986b
Hasibul Hasan Hasib <hasib.hasan@brainstation-23.com>
2025-03-25 11:59:27 +0600

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{     internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0) count++;
+            }
+            Console.WriteLine("total count is "+ count +'\n');
+        }
+    }
+}
---
+++ ConsoleApp2/Season.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp2
+{
+    public enum Season
+    {
+        summer,
+        rainy
+    }
+}
